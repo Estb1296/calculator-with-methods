9a2a3573fae96b8459cc9362c7e64c8066009bf7
ezrateshale <ezra.teshale12@gmail.com>
2026-04-10 14:58:43 -0500

    feat: Edit CompareNumbers.java and Format.java

--- src/main/java/com/pluralsight/CompareNumbers.java
+++ src/main/java/com/pluralsight/CompareNumbers.java
@@ -1,18 +1,29 @@
 package com.pluralsight;
+
 import java.util.Scanner;
+
 public class CompareNumbers {
     public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
-    System.out.print("Enter a number to check is it's even: ");
-    int number=input.nextInt();
-    System.out.println(isEven(number));
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number to check is it's even: ");
+        int number = input.nextInt();
+        if (isEven(number)) {
+            System.out.println("It is even.");
+        } else if (!(isEven(number))) {
+            System.out.println("It is odd.");
+        }
+        isPositive(number);
     }
-    public static boolean isEven(int number){
-       if(number%2==0){
-           System.out.println("It is even");
-           return true;
-       }
-        System.out.println("It isn't");
-        return false;
+
+    public static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+
+    public static void isPositive(double number) {
+        if (number > 0) {
+            System.out.println("It is positive");
+        } else {
+            System.out.println("It is negative");
+        }
     }
 }
--- src/main/java/com/pluralsight/Format.java
+++ src/main/java/com/pluralsight/Format.java
@@ -7,15 +7,17 @@ public class Format {
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter your name:");
         System.out.print("Enter your first name here: ");
-        String first=input.nextLine();
+        String first = input.nextLine();
         System.out.print("Enter your last name here: ");
-        String last=input.nextLine();
-       String call=formatName(last,first);
+        String last = input.nextLine();
+        System.out.println(formatName(first, last));
+        String call = formatName(first, last);
         System.out.println(call);
 
     }
-    public static String formatName(String first, String last){
-        String  message = ", how is your day?";
-        return first + ", " + last + message;
+
+    public static String formatName(String first, String last) {
+        String message = ", how is your day?";
+        return last + ", " + first + message;
     }
 }
